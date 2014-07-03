23c66bc0c223fbb3da973df33a524a17434cae10
tommaloney <tom.maloney@gmail.com>
2014-07-03 13:13:05 -0700

    Modified files pre-Ken

---
+++ chat.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<div id="page-wrap">
+
+		    <h2>jQuery/PHP Chat</h2>
+		    
+		    <p id="name-area"></p>
+		    
+		    <div id="chat-wrap"><div id="chat-area"></div></div>
+		    
+		    <form id="send-message-area">
+		        <p>Your message: </p>
+		        <textarea id="sendie" maxlength = '100'></textarea>
+		    </form>
+
+		</div>	
+	</body>
+</html>
